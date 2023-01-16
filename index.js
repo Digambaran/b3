4647626973b54f6f681c8967696ec008769ef713
Digambaran <arjunsreekumar.1@gmail.com>
2023-01-16 12:25:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const b3 = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello b3`}))
+  res.end()
+  
+}
+
+export default b3
